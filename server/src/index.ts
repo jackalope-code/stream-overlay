7ddbb81faee82c8b319f7d36bc80f2d304c5a969
Sean Phelan <seanphelan97@gmail.com>
2023-03-24 03:15:34 -0600

    replace apollo server code

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,9 @@
-import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
-import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
-import http from 'http';
-import { makeExecutableSchema } from '@graphql-tools/schema';
 import { WebSocketServer } from 'ws';
-import { useServer } from 'graphql-ws/lib/use/ws';
 import redis from 'redis'
 import express from 'express';
 import {Request} from 'express';
 import cors from 'cors';
-import bodyParser from 'body-parser';
-
-import { typeDefs, resolvers } from './schema.js';
+import expressWs from "express-ws";
 
 const SERVER_PORT = "4000";
 
@@ -23,69 +15,25 @@ function mockMovingCheck(id: any) {
   }
 }
 
-interface MyContext {
-  token?: string;
-}
+let appBase = express();
+let wsInstance = expressWs(appBase);
+let { app } = wsInstance; // let app = wsInstance.app;
 
-// Required logic for integrating with Express
-const app = express();
-// Our httpServer handles incoming requests to our Express app.
-// Below, we tell Apollo Server to "drain" this httpServer,
-// enabling our servers to shut down gracefully.
-const httpServer = http.createServer(app);
 
-const schema = makeExecutableSchema({ typeDefs, resolvers });
-
-// Creating the WebSocket server
-const wsServer = new WebSocketServer({
-  // This is the `httpServer` we created in a previous step.
-  server: httpServer,
-  // Pass a different path here if app.use
-  // serves expressMiddleware at a different path
-  path: '/graphql',
-});
-
-// Hand in the schema we just created and have the
-// WebSocketServer start listening.
-// eslint-disable-next-line react-hooks/rules-of-hooks
-const serverCleanup = useServer({ schema }, wsServer);
-
-const server = new ApolloServer({
-  schema,
-  plugins: [
-    // Proper shutdown for the HTTP server.
-    ApolloServerPluginDrainHttpServer({ httpServer }),
-    // Proper shutdown for the WebSocket server.
-    {
-      async serverWillStart() {
-        return {
-          async drainServer() {
-            await serverCleanup.dispose();
-          },
-        };
-      },
-    },
-  ],
-});
-
-// Ensure we wait for our server to start
-await server.start();
-
-// Set up our Express middleware to handle CORS, body parsing,
-// and our expressMiddleware function.
-app.use(
-  '/',
-  cors<cors.CorsRequest>({
+app.use(cors<cors.CorsRequest>(
+  {
     origin: "http://localhost:3000"
-  }),
-  bodyParser.json(),
-  // expressMiddleware accepts the same arguments:
-  // an Apollo Server instance and optional configuration options
-  expressMiddleware(server, {
-    context: async ({ req }) => ({ token: req.headers.token }),
-  }),
-);
+  })
+)
 
+// TODO: AUTH LAYER HERE
+
+app.ws('/', function(ws, req) {
+  ws.on('message', function(msg) {
+    console.log(msg);
+  });
+  console.log('socket', req.testing);
+});
 
 app.get("/components/:id", (req: Request<{id: string}>, res, err) => {
   res.status(200).json({id: req.params.id, moving: mockMovingCheck(req.params.id)})
@@ -95,7 +43,6 @@ app.get("/", (req, res, err) => {
   res.send("Hello");
 })
 
-// Modified server startup
-await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
-console.log(`🚀 Server ready at http://localhost:4000/`);
-app.use(cors());
+app.listen(SERVER_PORT, () => {
+  console.log(`Express server listening on port ${SERVER_PORT}`);
+})
