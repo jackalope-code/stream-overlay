6b79cb5dfb18203ddffe6476966451c0e04212ff
Sean Phelan <seanphelan97@gmail.com>
2023-03-25 07:08:42 -0600

    types, PUT route, refactor. routes still WIP

--- server/src/index.ts
+++ server/src/index.ts
@@ -27,32 +27,50 @@ const clients: WebSocket[] = [];
 
 // Track all networked components
 // TODO: There is no expectation of persistance
-interface ComponentParams {
-  url: string;
+interface ComponentCommonParams {
   width: number;
   height: number;
   x?: number;
   y?: number;
 }
-interface ComponentData extends ComponentParams{
-  id: string;
+interface ComponentUpdateParams extends ComponentCommonParams {
+  id: number;
 }
-const components: ComponentData[] = [];
+interface ComponentAllParams extends ComponentCommonParams {
+  url: string;
+};
+interface ComponentMap {
+  [key: string]: ComponentAllParams;
+}
+const components: ComponentMap = {};
 
+function broadcast(msg: WebSocket.RawData, ignoreClient?: WebSocket) {
+  if(ignoreClient !== undefined) {
+    for(let client of clients) {
+      if(client !== ignoreClient) {
+        client.send(msg)
+      }
+    }
+  } else {
+    for(let client of clients) {
+      client.send(msg)
+    }
+  }
+}
 // Forward update messages to other connected clients (websocket connection)
 app.ws('/', function(ws, req: Request<{}>) {
   clients.push(ws);
   ws.on('message', function(msg) {
     // TODO: vulnerable (naive) re-broadcasting
-    for(let client of clients) {
-      if(client !== ws) {
-        client.send(msg)
-      }
-    }
+    broadcast(msg, ws);
   });
 });
 
-// REST ROUTES NOT IMPLEMENTED BY THE CLIENT YET
+// UNTESTED REST ROUTES NOT IMPLEMENTED BY THE CLIENT YET
+// TODO: NONE OF THESE ARE BROADCASTING UPDATES AKA THEYRE BROKEN FOR NOW
+//    The client would get confused by receiving its own broadcast events and
+//    there's no ws variable scope here to filter that client out
+//    AAAUUUUUUUUGGGHH
 
 // Get all components for the session
 app.get("/components", (req, res) => {
@@ -60,12 +78,29 @@ app.get("/components", (req, res) => {
 });
 
 // Add a new component
-app.post("/component/:url/:width/:height/:x?/:y?", (req: Request<ComponentParams>, res) => {
+app.post("/component/:url/:width/:height/:x?/:y?", (req: Request<ComponentAllParams>, res) => {
   const id = randomUUID();
   const {url, width, height, x, y} = req.params;
-  components.push({id, url, width, height, x, y})
+  components[id] = {url, width, height, x, y};
   res.send(200);
-})
+});
+
+// Edit component fields except for url
+app.put("/component/:id/:width?/:height?/:x?/:y?", (req: Request<ComponentUpdateParams>, res) => {
+  const {id, width, height, x, y} = req.params;
+  if(id in components) {
+    const {
+      width: componentWidth,
+      height: componentHeight,
+      x: componentX,
+      y: componentY
+    } = components[id];
+    components[id].width = width || componentWidth;
+    components[id].height = height || componentHeight;
+    components[id].x = x || componentX;
+    components[id].y = y || componentY;
+  }
+});
 
 // Test endpoint
 app.get("/", (req, res, err) => {
