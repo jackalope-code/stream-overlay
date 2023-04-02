1a322ea88af998ad4accfa5ff43e6f5936baa447
Sean Phelan <seanphelan97@gmail.com>
2023-04-02 08:16:49 -0600

    f it remove env vars for now

--- src/utils.ts
+++ src/utils.ts
@@ -6,26 +6,26 @@ export function env() {
     throw new Error("Set REACT_APP_BUILD_FLAG to either dev or prod build.");
   }
 
-  let socketUrl;
-  let routeUrl;
-  let clientUrl;
+  let socketUrl = "ws://localhost:4000";
+  let routeUrl = "http://localhost:4000";
+  // let clientUrl;
 
-  if(buildType === 'dev') {
-    socketUrl = process.env.REACT_APP_DEV_WS_URL;
-    routeUrl = process.env.REACT_APP_DEV_REST_URL;
-  // } else if(buildType === 'prod') {
-  //   socketUrl = process.env.REACT_APP_WS_URL;
-  //   routeUrl = process.env.REACT_APP_REST_URL;
-  } else if(buildType === 'docker') {
-    routeUrl = 'http://api:4000'
-    socketUrl = 'ws://api:4000';
-  }
+  // if(buildType === 'dev') {
+  //   socketUrl = process.env.REACT_APP_DEV_WS_URL;
+  //   routeUrl = process.env.REACT_APP_DEV_REST_URL;
+  // // } else if(buildType === 'prod') {
+  // //   socketUrl = process.env.REACT_APP_WS_URL;
+  // //   routeUrl = process.env.REACT_APP_REST_URL;
+  // } else if(buildType === 'docker') {
+  //   routeUrl = 'http://api:4000'
+  //   socketUrl = 'ws://api:4000';
+  // }
   
-  if(!socketUrl || !routeUrl || !clientUrl) {
-    throw new Error("Could not locate environment variables. \
-    Requires REACT_APP_DEV_WS_URL, REACT_APP_LOCAL_CLIENT_URL, and REACT_APP_DEV_ROUTE_URL to be set.")
-  }
-  return {socketUrl, routeUrl, clientUrl}
+  // if(!socketUrl || !routeUrl) {
+  //   throw new Error("Could not locate environment variables. \
+  //   Requires REACT_APP_DEV_WS_URL, REACT_APP_LOCAL_CLIENT_URL, and REACT_APP_DEV_ROUTE_URL to be set.")
+  // }
+  // return {socketUrl, routeUrl}
 }
 
 // Used to update state with a new object to trigger re-rendering
