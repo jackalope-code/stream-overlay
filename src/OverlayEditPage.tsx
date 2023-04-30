0cedbb7db9f74246b3e93faba0e2bc886a0f4e8a
Sean Phelan <seanphelan97@gmail.com>
2023-04-30 00:27:16 -0600

    Connect auth

--- src/OverlayEditPage.tsx
+++ src/OverlayEditPage.tsx
@@ -5,6 +5,8 @@ import { env } from "./utils";
 import WidgetFieldForm from "./WidgetFieldForm";
 import WidgetPropertyForms from "./WidgetPropertyForms";
 import { FormikHelpers, FormikValues } from "formik";
+import { useAuth } from "./header-auth";
+import { EditorLoginForm } from "./EditorLoginForm";
 
 const routeUrl = env().routeUrl;
 
@@ -15,14 +17,16 @@ export default function OverlayEditPage() {
   // TODO: lifted state
   // Tracks all the draggable networked components in one object
   const [widgetDataMap, setWidgetDataMap] = useState<WidgetDataMap>({});
-  const [clientId, setClientId] = useState<string | undefined>();
   // TODO: addWidget possibly undefined for some reason
   // TODO: Can only be called once in top level... I want more of a context/provider use setup
-  const [_, helpers] = useOverlay(setWidgetDataMap, widgetDataMap, clientId);
+  const helpers = useOverlay(setWidgetDataMap);
   
   // TODO: IMPORTANT SET FROM OVERLAY REQUEST AND PREVENT NEW CLIENTS OVERRIDING
   const [overlayDimensions, setOverlayDimensions] = useState({width: 1920, height: 1080});
   const [formDimensionData, setFormDimensionData] = useState<{width: number, height: number}>(overlayDimensions);
+
+  // TODO: persist auth and make auth global
+  const {clientId, authenticate} = useAuth();
 
   // TODO: Form networked update hack
   useEffect(() => {
@@ -95,67 +99,75 @@ export default function OverlayEditPage() {
     formikHelpers.setSubmitting(false);
   }
   
-  // TODO: no space under the overlay or around the sides when it's larger 
-  // TODO: Separate out editor form properties
-  return (
-    <>
-      <form onSubmit={handleOverlayFormSubmit}>
-        <label>
-          <fieldset>
-            <legend>Overlay properties - WARNING! THIS CHANGES THE STREAM VIEW!</legend>
-            <label>
-              Width
-              <input type="number" name="overlayWidthField" value={formDimensionData.width}
-                defaultValue={overlayDimensions.width}
-                onChange={handleOverlayFormChange}
-              />
-            </label>
-            <label>
-              Height
-              <input type="number" name="overlayHeightField" value={formDimensionData.height}
-                defaultValue={overlayDimensions.height}
-                onChange={handleOverlayFormChange}
-              />
-            </label>
-            <input type="submit" value="Update"/>
-          </fieldset>
-        </label>
-      </form>
-      <form onSubmit={handleEditorFormSubmit}>
+  const pageContents = (
+  <>
+    <form onSubmit={handleOverlayFormSubmit}>
+      <label>
         <fieldset>
-          <legend>Editor properties - Change your editor view without changing what others see</legend>
+          <legend>Overlay properties - WARNING! THIS CHANGES THE STREAM VIEW!</legend>
           <label>
-            Scale
-            <input type="number" name="editorScale" step="0.1" min="0.1" max={3} defaultValue={editorScale}/>
+            Width
+            <input type="number" name="overlayWidthField" value={formDimensionData.width}
+              defaultValue={overlayDimensions.width}
+              onChange={handleOverlayFormChange}
+            />
           </label>
           <label>
-            Offset from top (%)
-            <input type="number" name="editorTranslateY" step="5" min={0} max={100} defaultValue={10}/>
+            Height
+            <input type="number" name="overlayHeightField" value={formDimensionData.height}
+              defaultValue={overlayDimensions.height}
+              onChange={handleOverlayFormChange}
+            />
           </label>
           <input type="submit" value="Update"/>
         </fieldset>
-      </form>
-      {/* TODO: FIX PROP DRILLING */}
-      <WidgetPropertyForms
+      </label>
+    </form>
+    <form onSubmit={handleEditorFormSubmit}>
+      <fieldset>
+        <legend>Editor properties - Change your editor view without changing what others see</legend>
+        <label>
+          Scale
+          <input type="number" name="editorScale" step="0.1" min="0.1" max={3} defaultValue={editorScale}/>
+        </label>
+        <label>
+          Offset from top (%)
+          <input type="number" name="editorTranslateY" step="5" min={0} max={100} defaultValue={10}/>
+        </label>
+        <input type="submit" value="Update"/>
+      </fieldset>
+    </form>
+    {/* TODO: FIX PROP DRILLING */}
+    <WidgetPropertyForms
+      widgetDataMap={widgetDataMap}
+      updateWidget={helpers.updateWidget as UseOverlayHelpers["updateWidget"]}
+      deleteWidget={helpers.deleteWidget as UseOverlayHelpers["deleteWidget"]}
+      clientId={clientId}
+    />
+    <WidgetFieldForm handleFormSubmit={handleAddWidgetFieldForm} buttonType="add"/>
+    <div style={{display: "flex", justifyContent: "center"}}>
+      <Overlay
+        dimensions={overlayDimensions}
         widgetDataMap={widgetDataMap}
-        updateWidget={helpers.updateWidget as UseOverlayHelpers["updateWidget"]}
-        deleteWidget={helpers.deleteWidget as UseOverlayHelpers["deleteWidget"]}
+        setWidgetDataMap={setWidgetDataMap}
+        setDimensions={setOverlayDimensions}
+        scale={editorScale} translateY={editorTranslateY}
+        style={{border: "solid red 1px"}}
         clientId={clientId}
       />
-      <WidgetFieldForm handleFormSubmit={handleAddWidgetFieldForm} buttonType="add"/>
-      <div style={{display: "flex", justifyContent: "center"}}>
-        <Overlay
-          dimensions={overlayDimensions}
-          widgetDataMap={widgetDataMap}
-          setWidgetDataMap={setWidgetDataMap}
-          setDimensions={setOverlayDimensions}
-          scale={editorScale} translateY={editorTranslateY}
-          style={{border: "solid red 1px"}}
-          setClientId={setClientId}
-          clientId={clientId}
-        />
-      </div>
-    </>
+    </div>
+  </>
+)
+  // TODO: no space under the overlay or around the sides when it's larger 
+  // TODO: Separate out editor form properties
+  return (
+    clientId === undefined ? (
+      <EditorLoginForm authenticate={authenticate}/>
+    )
+    :
+    (
+      pageContents
+    )
   )
 }
 
