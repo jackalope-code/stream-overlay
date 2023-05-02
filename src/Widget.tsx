c985ebc7e8acae36b774de2330cf738600ce21ff
Sean Phelan <seanphelan97@gmail.com>
2023-05-02 05:53:25 -0600

    Throttle drag events, set on drag, remove pointer events with css when dragging videos/embeds.

--- src/Widget.tsx
+++ src/Widget.tsx
@@ -1,9 +1,13 @@
 import axios from 'axios';
-import { MouseEventHandler, useState } from 'react';
+import { MouseEventHandler, useCallback, useState } from 'react';
 import Draggable, { DraggableEventHandler } from 'react-draggable';
 import { SendMessage } from 'react-use-websocket';
-import { WidgetDataMap } from './Overlay';
+import { WidgetData, WidgetDataMap } from './Overlay';
 import { copyAllWidgetData } from './utils';
+import { RateLimiter } from "limiter";
+import { debounce, throttle } from 'lodash';
+
+const dragUpdateLimiter = new RateLimiter({tokensPerInterval: 1, interval: 50});
 
 export interface WidgetProps {
   id: string;
@@ -32,6 +36,7 @@ const Widget: React.FC<WidgetProps> = ({id, owner, x, y, width, height, srcUrl, 
   // Unused state variable
   // https://react.dev/reference/react/useState
   const [disabled, setDisabled] = useState(false);
+  const [dragging, setDragging] = useState(false);
   
   const dragStartHandler: DraggableEventHandler = (e, data) => {
     // console.log("Drag start");
@@ -39,15 +44,31 @@ const Widget: React.FC<WidgetProps> = ({id, owner, x, y, width, height, srcUrl, 
 
   const dragStopHandler: DraggableEventHandler = (e, data) => {
     // console.log("Drag stop");
+    setDragging(false);
+    sendDragUpdate(buildWidget(data.x, data.y))
   }
 
+  function buildWidget(x: number, y: number) {
+    return {componentId: id, x, y, width, height}
+  }
+
+  const sendDragUpdate = useCallback(throttle((widgetData: {x: number, y: number}) => {
+    sendMessage(JSON.stringify({componentId: id, x: widgetData.x, y: widgetData.y, width, height}))
+  }, 50), []);
+
   const dragUpdateHandler: DraggableEventHandler = (e, data) => {
+    e.stopPropagation();
+    setDragging(true);
     setComponentData((prevState) => {
       const objCopy = copyAllWidgetData(prevState);
       const newData = Object.assign(objCopy, {[id]: {...objCopy[id], x: data.x, y: data.y}})
       return newData;
     })
-    sendMessage(JSON.stringify({componentId: id, x: data.x, y: data.y, width, height}))
+    const widget = {x: data.x, y: data.y, width, height};
+    sendDragUpdate(widget);
+    // dragUpdateLimiter.removeTokens(1).then(() => {
+    //   sendMessage(JSON.stringify({componentId: id, x: data.x, y: data.y, width, height}))
+    // })
   }
 
   const draggableStyling: React.CSSProperties = {
@@ -72,11 +93,11 @@ const Widget: React.FC<WidgetProps> = ({id, owner, x, y, width, height, srcUrl, 
         break;
       case WidgetType.Video:
       case WidgetType.Embed:
-        return <iframe width={width} height={height} src={srcUrl} />
+        return <iframe width={width} height={height} src={srcUrl} draggable="false" style={{pointerEvents: dragging ? 'none' : 'auto'}}/>
         break;
     }
   }
-  
+
   return (
     draggableChildren ? (
       <Draggable
